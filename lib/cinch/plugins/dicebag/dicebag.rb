56b86018d9ecfe3ee0bb1fcd9b5bc526cdacb780
Brian Haberer <bhaberer@gmail.com>
2013-06-07 19:06:11 -0700

    Adding Documantation

--- lib/cinch/plugins/dicebag/dicebag.rb
+++ lib/cinch/plugins/dicebag/dicebag.rb
@@ -5,6 +5,7 @@ require 'cinch-cooldown'
 require 'cinch-storage'
 require 'time-lord'
 
+
 module Cinch::Plugins
   class Dicebag
     include Cinch::Plugin
@@ -27,6 +28,10 @@ module Cinch::Plugins
       m.reply (dice.nil? ? roll_dicebag(m.user.nick, m.channel) : roll(m.user.nick, dice))
     end
 
+    # Roll a random assortment of dice, total the rolls, and record the score.
+    # @param [String] nick Nickname of the user rolling.
+    # @param [Cinch::Channel] channel The Channel object where the roll took place.
+    # @return [String] A description of the roll that took place
     def roll_dicebag(nick, channel)
       return "You must use that command in the main channel." if channel.nil?
 
@@ -41,6 +46,11 @@ module Cinch::Plugins
       return message
     end
 
+    # Roll a specific set of dice and return the pretty result
+    # @param [String] nick Nickname of the user rolling.
+    # @param [String] dice Space delimited string of dice to role.
+    #   (i.e. '6d12 4d20 d10'
+    # @return [String] String describing the dice that were rolled
     def roll(nick, dice)
       return nil if dice.nil? || nick.nil?
 
@@ -49,13 +59,12 @@ module Cinch::Plugins
       return "#{nick} rolls #{dice} totalling #{result}" unless result.nil?
     end
 
-    def roll_die(sides, count)
-      return 0 if sides < 1 || count < 1
-      total = 0
-      count.times { total += rand(sides) + 1 }
-      return total
-    end
 
+    # Takes an Array of dice rolls, sanitizes them, parses them, and dispatches
+    #   their calculation to `roll_die`.
+    # @param [Array] dice Array of strings that correspond to valid die rolls.
+    #   (i.e. ['4d6', '6d10']
+    # @return [Fixnum] The total from rolling all of the dice.
     def roll_dice(dice)
       # Clean out anything invalid
       dice.delete_if { |d| d.match(/\d*d\d+/).nil? }
@@ -74,6 +83,21 @@ module Cinch::Plugins
       return total
     end
 
+    # Rolls an n-sided die a given amount of times and returns the total
+    # @param [Fixnum] sides Number of sides of the die we're rolling.
+    # @param [Fixnum] count Number of times to roll the die.
+    # @return [Fixnum] The total from rolling the die.
+    def roll_die(sides, count)
+      return 0 if sides < 1 || count < 1
+      total = 0
+      count.times { total += rand(sides) + 1 }
+      return total
+    end
+
+    # Simple method to return a flavor text 'size' description based on
+    #   how many dice you happened to get in your dicebag roll.
+    # @param [Fixnum] size The number of dice in the dicebag.
+    # @return [String] Description of the size of the bag.
     def get_bag_size(size)
       case size
       when 0..100
@@ -91,6 +115,13 @@ module Cinch::Plugins
       end
     end
 
+    # Score checker for Dicebag rolls. Checks a given user/channel/score
+    #   against the current high score for that user.
+    # @param [String] nick Nickname of the user who rolled the score.
+    # @param [String] channel Name of the channel where the roll was made.
+    # @param [Fixnum] score The score from the bag.
+    # @return [String] If the new score is higher, returns an announcement
+    #   to that effect, otherwise returns a blank string.
     def score_check(nick, channel, score)
       # If the chennel or nick are not already initialized, spin them up
       @storage.data[channel] ||= Hash.new
